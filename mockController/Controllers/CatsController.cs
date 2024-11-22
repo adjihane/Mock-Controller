a3ea93c4429dcd385178242bb45ba6ffb889e986
Mathieu Briau <mathieu.briau@cegepmontpetit.ca>
2024-11-22 09:42:57 -0500

    Mise à jour des commentaires

--- mockController/Controllers/CatsController.cs
+++ mockController/Controllers/CatsController.cs
@@ -19,12 +19,12 @@ namespace mock.depart.Controllers
         public virtual string UserId { get { return User.FindFirstValue(ClaimTypes.NameIdentifier)!; } }
 
 
-        // TODO Pour facilité les tests il vaut mieux utiliser un ActionResult<Type>
+        // Pour facilité les tests il vaut mieux utiliser un ActionResult<Type>
         // DELETE: api/Cats/5
         [HttpDelete("{id}")]
         public ActionResult<Cat> DeleteCat(int id)
         {
-            // TODO vous devrez surement en faire une propriété pour pouvoir la "mock"
+            // UserId utilise une propriété pour pouvoir la "mock" facilement
             string? userid = UserId;
 
             // TODO vous devrez aussi faire un mock avec le service
