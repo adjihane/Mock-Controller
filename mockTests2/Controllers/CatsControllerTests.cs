10b245fba2a3e686dcfeb28105304242603b9a8d
j <2282988@cegepmontpetit.ca>
2025-05-05 10:14:42 -0400

    Test fini

--- mockTests2/Controllers/CatsControllerTests.cs
+++ mockTests2/Controllers/CatsControllerTests.cs
@@ -26,6 +26,7 @@ namespace mock.depart.Controllers.Tests
         public void DeleteCatNull()
         {
             Mock<CatsService> catServiceMock = new Mock<CatsService>();
+            //dans un test ou le controller va chercher un user il faut mocker le controller
             Mock<CatsController> catControllerMock = new Mock<CatsController>(catServiceMock.Object) { CallBase = true };
 
             catServiceMock.Setup(s => s.Get(It.IsAny<int>())).Returns((Cat?)null);
@@ -43,14 +44,24 @@ namespace mock.depart.Controllers.Tests
             //si le owner id != de userid
             Mock<CatsService> catServiceMock = new Mock<CatsService>();
             Mock<CatsController> catControllerMock = new Mock<CatsController>(catServiceMock.Object) { CallBase = true };
-            Mock<Cat> catMock = new Mock<Cat>();
+            Cat catMock = new Cat();
+            catMock.CatOwner = new CatOwner();
+            catMock.CatOwner.Id = "1";
+            catMock.Id = 1;
 
+            //mock UserId
             catControllerMock.Setup(foo => foo.UserId).Returns("2");
-            catMock.Setup(c => c.CatOwner).Returns(new CatOwner { Id = "1"});
-            catServiceMock.Setup(s => s.Delete(1)).Throws(new BadRequest("Cat is not yours"));
+
+
+            //catMock.Setup(c => c.CatOwner).Returns(new CatOwner { Id = "1"});
+
+
+            catServiceMock.Setup(s => s.Get(1)).Returns(catMock);
+
+            // catServiceMock.Setup(s => s.Delete(1)).Throws(new BadRequestResult());
 
             var actionresult = catControllerMock.Object.DeleteCat(1);
-            var result = actionresult.Result as BadRequestResult;
+            var result = actionresult.Result as BadRequestObjectResult;
             Assert.IsNotNull(result);
         }
 
@@ -60,14 +71,33 @@ namespace mock.depart.Controllers.Tests
             //delete bien le chat
             Mock<CatsService> catServiceMock = new Mock<CatsService>();
             Mock<CatsController> catControllerMock = new Mock<CatsController>(catServiceMock.Object) { CallBase = true };
+            //CatsController catsController = new CatsController(catServiceMock.Object);
 
-            catControllerMock.Setup(foo => foo.DeleteCat(1)).Returns());
+            Cat cat = new Cat() { CuteLevel = Cuteness.BarelyOk, CatOwner = new CatOwner() { Id = "1" } };
+
+            catServiceMock.Setup(s => s.Get(cat.Id)).Returns(cat);
+            catControllerMock.Setup(foo => foo.UserId).Returns("1");
+
+            var actionresult = catControllerMock.Object.DeleteCat(cat.Id);
+            var result = actionresult.Result as OkObjectResult;
+            Assert.IsNotNull(result);
         }
 
         [TestMethod()]
         public void DeleteTooCuteCat()
         {
             //si le chat est trop cute
+            Mock<CatsService> catServiceMock = new Mock<CatsService>();
+            Mock<CatsController> catControllerMock = new Mock<CatsController>(catServiceMock.Object) { CallBase = true };
+            Cat catTooCute = new Cat() { CuteLevel = Cuteness.Amazing, CatOwner = new CatOwner() { Id = "1" } };
+
+            catControllerMock.Setup(t => t.UserId).Returns("1");
+            catServiceMock.Setup(t => t.Get(catTooCute.Id)).Returns(catTooCute);
+
+            var actionresult = catControllerMock.Object.DeleteCat(catTooCute.Id);
+            var result = actionresult.Result as BadRequestObjectResult;
+            Assert.IsNotNull(result);
+
         }
     }
 }
